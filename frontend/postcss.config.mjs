f63e0146ed4dd0ab4cbfd318018fa7216f11e430
Rishi Ranjan <101rishidsr@gmail.com>
2026-05-07 07:53:05 +0530

    feat/nextjs installed

---
+++ frontend/postcss.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,mdx}",
+    "./pages/**/*.{js,jsx,mdx}",
+    "./components/**/*.{js,jsx,mdx}",
+    "./src/**/*.{js,jsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
